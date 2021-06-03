38542d28c5eb857d42e70981caeb4636d521670a
Aleks <alexdodul3@gmail.com>
2021-06-03 19:26:13 +0300

    added branch 1-4, Task 5.1 and 5.2

---
+++ src/main/java/hotel/dto/UpdateUserRequest.java
@@ -0,0 +1,17 @@
+package hotel.dto;
+
+import hotel.UserRole;
+
+public class UpdateUserRequest {
+
+    private String phone;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private UserRole role;
+
+}
